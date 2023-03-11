a83406c1eeb4a135425e147f46439d7e74b4c305
atopala7 <atopala7@gmail.com>
2023-03-11 13:38:10 -0500

    Created sidebar

--- react-spotify-app/src/pages/Root.js
+++ react-spotify-app/src/pages/Root.js
@@ -14,23 +14,35 @@ import { getCurrentlyPlaying } from '../spotify';
 import { accessToken, logout } from '../spotify';
 import { catchErrors } from '../utils';
 
+import '../styles/root.css'
+
 const Root = () => {   
     return (
         <>
-            <header className="App-header">
-                <nav>
-                <NavLink to="/" className="Styled-button">Home</NavLink>
-                {/* <NavLink to={{pathname: "/data"}}
-                            state={{ song: data }}
-                             className="Styled-button">Data</NavLink> */}
-                <NavLink to="/information" className="Styled-button">Information</NavLink>
-                <NavLink to="/lyrics" className="Styled-button">Lyrics</NavLink>
-                <NavLink to="/profile" className="Styled-button">Profile</NavLink>
-                <NavLink to="/logout" className="Styled-button Logout-button">Log Out</NavLink>
-                {/* <NavLink to="/logout" className="Styled-button Logout-button" onClick={logout}>Log Out</NavLink> */}
-                </nav>
-            </header>
-            <Data />
+        <div className="root">
+            <div className="contents">
+                <header className="App-header">
+                    <nav>
+                    <NavLink to="/" className="Styled-button">Home</NavLink>
+                    {/* <NavLink to={{pathname: "/data"}}
+                                state={{ song: data }}
+                                className="Styled-button">Data</NavLink> */}
+                    <NavLink to="/information" className="Styled-button">Information</NavLink>
+                    <NavLink to="/lyrics" className="Styled-button">Lyrics</NavLink>
+                    <NavLink to="/profile" className="Styled-button">Profile</NavLink>
+                    <NavLink to="/logout" className="Styled-button Logout-button">Log Out</NavLink>
+                    {/* <NavLink to="/logout" className="Styled-button Logout-button" onClick={logout}>Log Out</NavLink> */}
+                    </nav>
+                </header>
+                
+                <Data />
+            </div>
+            <div id="sidebar">
+                <p>
+                    Sidebar
+                </p>
+            </div>
+        </div>
         </>
     )
 };
