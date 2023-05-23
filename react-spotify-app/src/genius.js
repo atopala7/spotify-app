f06429fa82647bb7dcf413146a88936507032c1c
atopala7 <atopala7@gmail.com>
2023-05-23 17:22:35 -0400

    Setting up Genius API

---
+++ react-spotify-app/src/genius.js
@@ -0,0 +1,4 @@
+const client_id = "T4DXzLwLDjFhcx3jPsd7BVK_HhR8NfNa69HatonDHw6Qsli1qgx1v6bt94MNdHxm";
+const client_secret = "wZKXRevfQUvjk0mjiFZAWN_TbTUXB09bWrW65m4zkq7_f5xmyBMYnbU-paNwQiRMGo_OoGoXilNRsRM6bFovdg";
+
+const client_token = "NPfa4dvGEVBq9tbjiR-9yoOBb9HfRSgE_I_uBcwsdef1TZxs2zDUToh082pMvmwf";
