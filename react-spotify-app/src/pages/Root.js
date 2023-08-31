491747848b681e5fd9d8348c800142e0bd4c7436
atopala7 <atopala7@gmail.com>
2023-08-31 09:59:22 -0400

    Put everything inside a <main> tag

--- react-spotify-app/src/pages/Root.js
+++ react-spotify-app/src/pages/Root.js
@@ -37,64 +37,66 @@ const Root = () => {
 
     return (
         <>
-        {/* Navbar */}
-        <nav className="navbar navbar-expand-md navbar-light bg-secondary sticky-top">
-            <div className="container-xxl">
-                <a href="#" className="navbar-brand">
-                    <i className="bi bi-music-note-beamed text-dark"></i>
-                    <span className="fw-bold text-light">
-                        This Song
-                    </span>
-                </a>
-                {/* Toggle button for mobile nav */}
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+        <main>
+            {/* Navbar */}
+            <nav className="navbar navbar-expand-md navbar-light bg-secondary sticky-top">
+                <div className="container-xxl">
+                    <a href="#" className="navbar-brand">
+                        <i className="bi bi-music-note-beamed text-dark"></i>
+                        <span className="fw-bold text-light">
+                            This Song
+                        </span>
+                    </a>
+                    {/* Toggle button for mobile nav */}
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-                {/* Navbar links */}
-                {/* <div className="collapse navbar-collapse justify-content-start align-center" id="main-nav"> */}
-                <div className="nav nav-underline border-0 collapse navbar-collapse justify-content-start align-center" id="main-nav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <NavLink to="/" className="nav-link">Home</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/information" className="nav-link">Information</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/lyrics" className="nav-link">Lyrics</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/profile" className="nav-link">Profile</NavLink>
-                        </li>
-                        <li className="nav-item d-md-none">
-                            <NavLink to="/logout" className="nav-link">Log out</NavLink>
-                        </li>
-                        <li className="nav-item ms-2 d-none d-md-inline">
-                            <NavLink to="/logout" className="btn btn-outline-danger">Log out</NavLink>
-                        </li>
-                    </ul>
+                    {/* Navbar links */}
+                    {/* <div className="collapse navbar-collapse justify-content-start align-center" id="main-nav"> */}
+                    <div className="nav nav-underline border-0 collapse navbar-collapse justify-content-start align-center" id="main-nav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink to="/" className="nav-link">Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/information" className="nav-link">Information</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/lyrics" className="nav-link">Lyrics</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/profile" className="nav-link">Profile</NavLink>
+                            </li>
+                            <li className="nav-item d-md-none">
+                                <NavLink to="/logout" className="nav-link">Log out</NavLink>
+                            </li>
+                            <li className="nav-item ms-2 d-none d-md-inline">
+                                <NavLink to="/logout" className="btn btn-outline-danger">Log out</NavLink>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
 
-        {/* Main data component */}
-        <section id="data" className="bg-light">
-            <div className="container-lg">
-                <div className="rounded">
-                    <Data selectedSong={song} rootSelectSong={selectSong} />
+            {/* Main data component */}
+            <section id="data" className="bg-light">
+                <div className="container-lg">
+                    <div className="rounded">
+                        <Data selectedSong={song} rootSelectSong={selectSong} />
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
 
-        <div id="sidebar" onMouseOver={() => {
-            openSidebar();
-        }}
-        onMouseOut={() => {
-            closeSidebar();
-        }}>
-            <Recent rootSelectSong={selectSong} />
-        </div>
+            <div id="sidebar" onMouseOver={() => {
+                openSidebar();
+            }}
+            onMouseOut={() => {
+                closeSidebar();
+            }}>
+                <Recent rootSelectSong={selectSong} />
+            </div>
+        </main>
         </>
     )
 };
