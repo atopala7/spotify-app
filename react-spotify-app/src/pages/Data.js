47fb58be79ed6bd879f0cf32bbc6b573f7b51b3f
atopala7 <atopala7@gmail.com>
2023-10-19 16:12:40 -0400

    Album art darkens on hover

--- react-spotify-app/src/pages/Data.js
+++ react-spotify-app/src/pages/Data.js
@@ -8,127 +8,132 @@ import "../styles/data.css";
 import refresh from "../images/refresh.png";
 
 const Data = (props) => {
-    /**
-     * STATE VARIABLES
-     * Data is the Spotify data returned by the https://api.spotify.com/v1/me/player/currently-playing endpoint
-     * Status is the response status, which if 204 indicates that no song is currently playing
-     */
-    const [data, setData] = useState(null);
-    const [status, setStatus] = useState(null);
-    const [song, setSong] = useState(null);
+  /**
+   * STATE VARIABLES
+   * Data is the Spotify data returned by the https://api.spotify.com/v1/me/player/currently-playing endpoint
+   * Status is the response status, which if 204 indicates that no song is currently playing
+   */
+  const [data, setData] = useState(null);
+  const [status, setStatus] = useState(null);
+  const [song, setSong] = useState(null);
 
-    useEffect(() => {
-        getSong(props.selectedSong);
-    }, [props.selectedSong]);
+  useEffect(() => {
+    getSong(props.selectedSong);
+  }, [props.selectedSong]);
 
-    useEffect(() => {
-        if (data) {
-            const thisSong = {
-                albumArt: data.item.album.images[1].url,
-                songName: data.item.name,
-                artists: data.item.artists,
-                albumName: data.item.album.name
-            };
+  useEffect(() => {
+    if (data) {
+      const thisSong = {
+        albumArt: data.item.album.images[1].url,
+        songName: data.item.name,
+        artists: data.item.artists,
+        albumName: data.item.album.name
+      };
 
-            setSong(thisSong);
+      setSong(thisSong);
 
-            props.rootSelectSong(thisSong);
-        }
-    }, [data]);
+      props.rootSelectSong(thisSong);
+    }
+  }, [data]);
 
-    // Awaits the song that's currently playing and sets state variables accordingly
-    const getSong = (select) => {
-        // Clear the previous state variables
-        setData(null);
-        setStatus(null);
+  // Awaits the song that's currently playing and sets state variables accordingly
+  const getSong = (select) => {
+    // Clear the previous state variables
+    setData(null);
+    setStatus(null);
 
-        if (!select) {
-            const fetchData = async () => {
-                const currentlyPlaying = await getCurrentlyPlaying();
-                setData(currentlyPlaying.data);
-                setStatus(currentlyPlaying.status);
+    if (!select) {
+      const fetchData = async () => {
+        const currentlyPlaying = await getCurrentlyPlaying();
+        setData(currentlyPlaying.data);
+        setStatus(currentlyPlaying.status);
 
-                console.log(data);
+        console.log(data);
 
-                if (!data) setSong(null);
-            };
-            catchErrors(fetchData());
-        } else {
-            console.log(select);
+        if (!data) setSong(null);
+      };
+      catchErrors(fetchData());
+    } else {
+      console.log(select);
 
-            const thisSong = {
-                albumArt: select.albumArt,
-                songName: select.songName,
-                artists: select.artists,
-                albumName: select.albumName
-            };
+      const thisSong = {
+        albumArt: select.albumArt,
+        songName: select.songName,
+        artists: select.artists,
+        albumName: select.albumName
+      };
 
-            setSong(thisSong);
-        }
-    };
+      setSong(thisSong);
+    }
+  };
 
-    return (
+  return (
+    <>
+      {(song && (
         <>
-            {(song && (
-                <>
-                    <div className="container-lg">
-                        <div className="row justify-content-center align-items-center">
-                            <div class="col-md-4 text-center d-flex justify-content-center justify-content-md-end align-items-center">
-                                <div
-                                    className="data-image-container"
-                                    onClick={() => {
-                                        getSong(null);
-                                    }}
-                                    onMouseOver={() => {
-                                        // document.querySelector(".data-image-container").style.setProperty("--refreshWidth", "150px");
-                                        // document.querySelector(".data-image-container").style.setProperty("--refreshOffset", "calc(var(--containerWidth) * 0.5 - var(--refreshWidth) * 0.5");
-                                        // document.querySelector(".data-image-container").style.setProperty("--refreshOrigin", "calc(var(--refreshWidth) * 0.5 + var(--refreshOffset)) calc(var(--refreshWidth) * 0.5  + var(--refreshOffset))");
-                                    }}
-                                >
-                                    <img
-                                        className="data-image"
-                                        src={song.albumArt}
-                                    />
-                                    {/* <img className='data-refresh' src={refresh} /> */}
-                                    {/* TODO: Or remove the refresh function entirely.
+          <div className="container-lg">
+            <div className="row justify-content-center align-items-center">
+              <div class="col-md-4 text-center d-flex justify-content-center justify-content-md-end align-items-center">
+                <div
+                  className="data-image-container"
+                  onClick={() => {
+                    getSong(null);
+                  }}
+                  onMouseOver={() => {
+                    // document
+                    //   .querySelector(".data-image-container")
+                    //   .style.setProperty("--refreshWidth", "150px");
+                    // document
+                    //   .querySelector(".data-image-container")
+                    //   .style.setProperty(
+                    //     "--refreshOffset",
+                    //     "calc(var(--containerWidth) * 0.5 - var(--refreshWidth) * 0.5"
+                    //   );
+                    // document
+                    //   .querySelector(".data-image-container")
+                    //   .style.setProperty(
+                    //     "--refreshOrigin",
+                    //     "calc(var(--refreshWidth) * 0.5 + var(--refreshOffset)) calc(var(--refreshWidth) * 0.5  + var(--refreshOffset))"
+                    //   );
+                  }}
+                >
+                  <img className="data-image" src={song.albumArt} />
+                  {/* <img className='data-refresh' src={refresh} /> */}
+                  {/* TODO: Or remove the refresh function entirely.
                                 Instead, on a web browser, use the album art as play/pause */}
-                                </div>
-                            </div>
-                            <div className="data-info col-md-8 text-center text-md-start">
-                                <h1>{song.songName}</h1>
-                                <h2>
-                                    {song.artists
-                                        .map((artist) => artist.name)
-                                        .join(", ")}
-                                </h2>
-                                <h2>{song.albumName}</h2>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="outlet-container card">
-                        <Outlet context={song} />
-                    </div>
-                </>
-            )) ||
-                (status == 204 && (
-                    <>
-                        <p>No song is currently playing.</p>
-                        <Outlet />
-                    </>
-                )) ||
-                (status >= 400 && (
-                    <>
-                        <p>Error retrieving data from Spotify.</p>
-                        <Outlet />
-                    </>
-                )) || (
-                    <>
-                        <p>Loading currently playing song...</p>
-                        <Outlet />
-                    </>
-                )}
+                </div>
+              </div>
+              <div className="data-info col-md-8 text-center text-md-start">
+                <h1>{song.songName}</h1>
+                <h2>{song.artists.map((artist) => artist.name).join(", ")}</h2>
+                <h2>{song.albumName}</h2>
+              </div>
+            </div>
+          </div>
+          <div className="outlet-container card">
+            <Outlet context={song} />
+          </div>
         </>
-    );
+      )) ||
+        (status == 204 && (
+          <>
+            <p>No song is currently playing.</p>
+            <Outlet />
+          </>
+        )) ||
+        (status >= 400 && (
+          <>
+            <p>Error retrieving data from Spotify.</p>
+            <Outlet />
+          </>
+        )) || (
+          <>
+            <p>Loading currently playing song...</p>
+            <Outlet />
+          </>
+        )}
+    </>
+  );
 };
 
 export default Data;
