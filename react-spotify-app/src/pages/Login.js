6cbbf5604cca72b9903622da2a19dcca6751dd91
atopala7 <atopala7@gmail.com>
2023-10-26 13:26:35 -0400

    Changed background color

--- react-spotify-app/src/pages/Login.js
+++ react-spotify-app/src/pages/Login.js
@@ -2,8 +2,8 @@ import logo from "../images/logo.svg";
 
 const Login = () => (
   <>
-    <main class="container-lg bg-dark vh-100">
-      <div class="justify-content-center align-items-center text-center">
+    <main className="bg-dark vh-100">
+      <div class="justify-content-center align-items-center text-center container-lg">
         <img src={logo} className="App-logo" alt="logo" />
         <p class="text-light">Spotify App</p>
         <a
