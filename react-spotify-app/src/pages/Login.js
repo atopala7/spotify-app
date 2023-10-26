d04a0e7bc36967d538f67fa6523dd22ec887641c
atopala7 <atopala7@gmail.com>
2023-10-26 11:08:13 -0400

    Styling login page

--- react-spotify-app/src/pages/Login.js
+++ react-spotify-app/src/pages/Login.js
@@ -1,17 +1,21 @@
 import logo from "../images/logo.svg";
 
 const Login = () => (
-    <>
+  <>
+    <main class="container-lg">
+      <div class="justify-content-center align-items-center">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Spotify App</p>
         <a
-            className="Login-button"
-            href="http://192.168.4.158:8888/login"
-            rel="noopener noreferrer"
+          className="Login-button"
+          href="http://192.168.4.158:8888/login"
+          rel="noopener noreferrer"
         >
-            Log in to Spotify
+          Log in to Spotify
         </a>
-    </>
+      </div>
+    </main>
+  </>
 );
 
 export default Login;
