266e40ad6062dbfc549619556b91540596dfa3e5
atopala7 <atopala7@gmail.com>
2023-10-27 01:59:44 -0400

    Switched to Heroku remote URL

--- react-spotify-app/src/musixmatch.js
+++ react-spotify-app/src/musixmatch.js
@@ -1,24 +1,30 @@
-import axios from 'axios';
+import axios from "axios";
 
 const PORT = 8888;
 
-const URL = "http://192.168.4.158:8888/musixmatch";
+// const URL = "http://192.168.4.158:8888/musixmatch";
+const URL =
+  "https://mysterious-garden-90298-d115e921537b.herokuapp.com/musixmatch";
 
 export const getLyrics = async (trackName, artists, albumName) => {
-    console.log(`Getting lyrics for ${trackName} by ${artists[0].name}`);
-    const lyrics = [];
+  console.log(`Getting lyrics for ${trackName} by ${artists[0].name}`);
+  const lyrics = [];
 
-    const paramaters = new URLSearchParams([["trackName", trackName], ["artistName", artists[0].name], ["albumName", albumName]]);
+  const paramaters = new URLSearchParams([
+    ["trackName", trackName],
+    ["artistName", artists[0].name],
+    ["albumName", albumName]
+  ]);
 
-    // Await the completion of the `/musixmatch` endpoint from the Node app
-    const response = await axios({
-        method: "get",
-        url: URL,
-        params: paramaters,
-        headers: {
-            "content-type": "application/x-www-form-urlencoded",
-        }
-    });
+  // Await the completion of the `/musixmatch` endpoint from the Node app
+  const response = await axios({
+    method: "get",
+    url: URL,
+    params: paramaters,
+    headers: {
+      "content-type": "application/x-www-form-urlencoded"
+    }
+  });
 
-    return response;
-}
+  return response;
+};
